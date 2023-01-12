22f23a43bc1fde3705981efef5e4843c230762f5
Vit <privit@rambler.ru>
2023-01-13 02:00:39 +0300

    Задание 1. (поправка на ветер)...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import transport.Car;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Car car [] = new Car[5];
+        car [0] = new Car("", "Mark II", 1992, "седан", 5,3.0,
+                "МКПП","White Pearl", "Япония", "");
+        car [1] = new Car("Audi", null, 2020, "седан", 5,3.0,
+                "МКПП","Чёрный", "Германия", "п234на161");
+        car [2] = new Car("BMW", "Z8", 2021, "седан", 0,3.0,
+                "АКПП"," ", "Германия", "у054ра178");
+        car [3] = new Car("Kia", "Sportage IV", 2018, null, 5,-2,
+                "","Красный", "Южная Корея", "л543на23");
+        car [4] = new Car("Hyundai", "Avante", 0, "седан", 5,1.6,
+                "МКПП","Оранжевый", null, "а100аа27");
+
+        for (int i = 0; i < car.length; i++) {
+            System.out.println(car[i]);
+            System.out.println("\t " + car[i].changeTires(0) + "\n");
+        }
     }
 }
--- src/transport/Car.java
+++ src/transport/Car.java
@@ -1,2 +1,102 @@
-package transport;public class Car {
+package transport;
+
+public class Car {
+    private final String brand;
+    private final String model;
+    private final int productionYear;
+    private final String carBodyType;
+    private final int numberOfSeats;
+    private double engineVolume;
+    private String transmission;
+    private String colour;
+    private final String productionCountry;
+    private String registrationNumber;
+
+    public Car (String brand, String model, int productionYear, String carBodyType, int numberOfSeats,
+                double engineVolume, String transmission, String colour, String productionCountry,
+                String registrationNumber) {
+
+        this.brand = (brand == null || brand.isBlank()) ? "Автомобиль " : brand;
+        this.model = (model == null || model.isBlank()) ? "" : model;
+        this.productionYear = (productionYear <=0 ) ? 2000 : productionYear;
+        this.carBodyType = (carBodyType == null || carBodyType.isBlank()) ? "custom" : carBodyType;
+        this.numberOfSeats = (numberOfSeats <=0 ) ? 4 : numberOfSeats;
+        this.setEngineVolume(engineVolume);
+        this.setTransmission(transmission);
+        this.setColour(colour);
+        this.productionCountry = (productionCountry == null || productionCountry.isBlank())
+                ? "Made in ..." : productionCountry;
+        this.setRegistrationNumber(registrationNumber);
+
+    }
+
+    public void setEngineVolume(double engineVolume) {
+        this.engineVolume = (engineVolume <= 0) ? 1.5 : engineVolume;
+    }
+    public void setTransmission(String transmission) {
+        this.transmission = (transmission == null || transmission.isBlank()) ? "top secret" : transmission;
+    }
+    public void setColour(String colour) {
+        this.colour = (colour == null || colour.isBlank()) ? "Белый" : colour;
+    }
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = (registrationNumber == null || registrationNumber.isBlank())
+                ? "а000аа49" : registrationNumber;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+    public String getModel() {
+        return model;
+    }
+    public int getProductionYear() {
+        return productionYear;
+    }
+    public String getCarBodyType() {
+        return carBodyType;
+    }
+    public int getNumberOfSeats() {
+        return numberOfSeats;
+    }
+    public double getEngineVolume() {
+        return engineVolume;
+    }
+    public String getTransmission() {
+        return transmission;
+    }
+    public String getColour() {
+        return colour;
+    }
+    public String getProductionCountry() {
+        return productionCountry;
+    }
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public String changeTires (int month) {
+        if (month >= 4 && month <= 11) {
+            return "на летней резине...";
+        } else {
+            if (month <= 0 || month > 12) {
+                return ";) всесезонка...";
+            } else {
+                return "на зимней резине...";
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return brand +" " + model + '\n' +
+                "\t год выпуска - " + productionYear + "г.\n" +
+                "\t тип кузова - '" + carBodyType + "'\n" +
+                "\t количество мест - " + numberOfSeats + "\n" +
+                "\t объём двигателя - " + engineVolume + " л\n " +
+                "\t трансмиссия - '" + transmission + "'\n" +
+                "\t цвет кузова - '" + colour + "'\n" +
+                "\t страна сборки - '" + productionCountry + "'\n" +
+                "\t регистрационный знак - " + registrationNumber;
+    }
 }
